9aabe2e02f62bc79b5bab99853d800b69c636044
ZHENYI BI （毕振义） <zhenyibi@trip.com>
2022-01-16 22:36:35 +0800

    dev_bzy commit ; collectionsDemo and collectionIteratorDemo modify while structure;

--- gitTest/src/com/collection/test/CollectionIteratorDemo.java
+++ gitTest/src/com/collection/test/CollectionIteratorDemo.java
@@ -22,6 +22,14 @@ public class CollectionIteratorDemo {
         System.out.println(it.next()); //NoSuchElementException表示被请求的元素不存在 */
 
         //boolean hasNext():如果迭代具有更多元素，则返回true
+        Iterator<String> it = c.iterator();
+        /*
+        遍历集合
+        while(it.hasNext()){
+            String s = it.next();
+            System.out.println("s");
+        }
+         */
         for (String s : c) {
             System.out.println(s);
         }
