b8d5c5a65941e4375f80a2eed718d825e5cc561c
ZHENYI BI （毕振义） <zhenyibi@trip.com>
2022-01-18 22:41:56 +0800

    dev_bzy commit ; LinkedListDemo learning;

---
+++ gitTest/src/com/collection/test/LinkedListDemo/LinkedListDemo.java
@@ -0,0 +1,22 @@
+package com.collection.test.LinkedListDemo;
+
+/*
+    List集合常用子类：ArrayList，LinkedList
+        ArrayList：底层是数组，查询快，增删慢
+        LinkedList：底层是链表，查询慢，增删快
+ */
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+    public static void main(String[] args) {
+        LinkedList<String> linedList = new LinkedList<>();
+        linedList.add("hello");
+        linedList.add("world");
+        linedList.add("test");
+        
+        for (String s : linedList) {
+            System.out.println(s);
+        }
+    }
+}
