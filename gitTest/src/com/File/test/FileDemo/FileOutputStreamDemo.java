1ecd33aa10c40555e59e7459f67454dccf90ae97
ZHENYI BI （毕振义） <zhenyibi@trip.com>
2022-02-21 23:28:27 +0800

    dev_bzy commit ; FileOutputStreamDemo learning adjust format;

--- gitTest/src/com/File/test/FileDemo/FileOutputStreamDemo.java
+++ gitTest/src/com/File/test/FileDemo/FileOutputStreamDemo.java
@@ -34,7 +34,7 @@ public class FileOutputStreamDemo {
         fos.write(bytes);
 
         //void write(byte[] b, int off, int len): 将len字节从指定的字节数组开始，从偏移量off开始写入此文件输出流，
-        fos.write(bytes,0,bytes.length-1);
+        fos.write(bytes, 0, bytes.length - 1);
 
         //释放资源
         fos.close();
