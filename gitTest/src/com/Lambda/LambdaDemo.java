906ec70ef1f4e40131a18b391713c4542a866ce7
ZHENYI BI （毕振义） <zhenyibi@trip.com>
2022-07-19 22:53:29 +0800

    LambdaDemo 实现类/匿名内部类/lambda表达式实现多线程

---
+++ gitTest/src/com/Lambda/LambdaDemo.java
@@ -0,0 +1,30 @@
+package com.Lambda;
+
+/**
+ * @author zhenyiBi
+ * @date 2022/7/19
+ * 需求：启动一个线程，在控制台输出一句话：多线程启动了
+ */
+public class LambdaDemo {
+    public static void main(String[] args) {
+
+        //实现类的方式实现需求，步骤繁琐
+//        MyRunnable my = new MyRunnable();
+//        Thread t = new Thread(my);
+//        t.start();
+
+        //匿名内部类改进，但是格式有点复杂
+//        new Thread(new Runnable(){
+//            @Override
+//            public void run() {
+//                System.out.println("多线程程序启动了！");
+//            }
+//        }).start();
+
+        //Lambda表达式改进
+        new Thread( () -> { 
+            System.out.println("多线程程序启动了！");
+        } ).start();
+
+    }
+}
---
+++ gitTest/src/com/Lambda/MyRunnable.java
@@ -0,0 +1,13 @@
+package com.Lambda;
+
+/**
+ * @author zhenyiBi
+ * @date 2022/7/19
+ */
+public class MyRunnable implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println("多线程程序启动了！");
+    }
+}
